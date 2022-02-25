a08944965f56474628031e7a4b2ad98aa89ffe9a
omar.ben.haddi <omar.ben.haddi@CPX-G3EEG1B1URZ.dir.svc.accenture.com>
2022-02-25 18:36:45 +0100

    EXTENDFEED-15: Remove drop/delete sql function

--- src/sql/plugins/extend/modules/feedback/upgrade/update_db_extend_feedback-1.0.4-2.0.0.sql
+++ src/sql/plugins/extend/modules/feedback/upgrade/update_db_extend_feedback-1.0.4-2.0.0.sql
@@ -1,7 +1,6 @@
 --
 -- Table structure for table extend_feedback
 --
-DROP TABLE IF EXISTS extend_feedback;
 CREATE TABLE extend_feedback (
   id INT NOT NULL AUTO_INCREMENT,
   id_history INT DEFAULT 0 NOT NULL,
@@ -17,7 +16,6 @@ CREATE TABLE extend_feedback (
 --
 -- Table structure for table extend_feedback_type
 --
-DROP TABLE IF EXISTS extend_feedback_type;
 CREATE TABLE extend_feedback_type (
   id INT NOT NULL AUTO_INCREMENT,
   label LONG VARCHAR NOT NULL,
@@ -42,6 +40,5 @@ ADD COLUMN id_workflow INT DEFAULT 0 NOT NULL;
 --
 -- Data for table core_admin_right
 --
-DELETE FROM core_admin_right WHERE id_right = 'FEEDBACK_TYPE_MANAGEMENT';
 INSERT INTO core_admin_right (id_right,name,level_right,admin_url,description,is_updatable,plugin_name,id_feature_group,icon_url,documentation_url, id_order ) VALUES 
 ('FEEDBACK_TYPE_MANAGEMENT','module.extend.feedback.module.manageFeedbackType.name',1,'jsp/admin/plugins/extend/modules/feedback/ManageFeedbackType.jsp','module.extend.feedback.module.manageFeedbackType.description',0,'extend-feedback','MANAGERS',NULL,NULL,4)
