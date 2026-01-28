c9da623a10dc62b652fc5a0d8cd3a19b967620ce
evdngsl <158455076+evdngsl@users.noreply.github.com>
2026-01-28 09:56:15 +0100

    LUT-31761 : Add the possibility to manage sql updates with liquibase plugin

--- src/sql/plugins/extend/modules/feedback/plugin/init_db_extend_feedback.sql
+++ src/sql/plugins/extend/modules/feedback/plugin/init_db_extend_feedback.sql
@@ -1,1 +1,4 @@
+-- liquibase formatted sql
+-- changeset extend-feedback:init_db_extend_feedback.sql
+-- preconditions onFail:MARK_RAN onError:WARN
 INSERT INTO extend_feedback_config(id_extender,message,id_mailing_list) VALUES (-1,'Vous avez une remarque à faire, une expérience à raconter sur le contenu de la page ou sur le service dont il est question ?',1);
